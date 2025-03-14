30173659f13c837daa2540fc945043768d8301b8
cyzuko <mateus23viana@gmail.com>
2025-03-14 10:48:28 +0000

    atualizado ig

---
+++ src/main/java/com/example/picaria_ldp/Main.java
@@ -0,0 +1,46 @@
+package main.java.com.example.picaria_ldp;
+
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+
+
+public class Main {
+
+    public static Game game = new Game();
+    public static Socket s;
+    public static Stage stagee;
+    public static int ServerPort;
+    public InetAddress ip;
+    public static boolean esperaJogo = false;
+    public static boolean SouPlayerUm;
+    public static LinkedList<Cliente> Jogadores= new LinkedList<Cliente>();
+    public static Cliente cliente;
+
+    /**
+     * É o main
+     * @param args
+     * @throws UnknownHostException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws UnknownHostException, IOException {
+
+
+        Thread rodaGame = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                game.main(args);
+
+            }
+        });
+
+        rodaGame.start();
+
+    }
+
+}
---
+++ src/main/java/com/example/picaria_ldp/howToController.java
@@ -0,0 +1,39 @@
+package main.java.com.example.picaria_ldp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class howToController {
+
+    private Stage stage;
+    private Scene scene;
+
+
+
+    /**
+     * Este método é para voltar para o FXML do Menu
+     * @param event O ActionEvent é registado ao clicar no botao
+     * @throws IOException Caso exista algum erro ao carregar o FXML.
+     */
+    @FXML
+    protected void voltar(ActionEvent event) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
