24d46622cdc329d3204a4dc027cf2a82a0f81783
cyzuko <mateus23viana@gmail.com>
2025-06-04 12:32:38 +0100

    Update ao Servidor

--- src/ServidorGlobal.java
+++ src/ServidorGlobal.java
@@ -122,7 +122,7 @@ public class ServidorGlobal {
                     }
 
 
-                    //jogada do segundo jogador
+                    //jogada do segundo jogadorr
                     if(recebido.startsWith("P2")){
 
                         System.out.println(this.name + ": Jogou esta jogada = " + recebido);
